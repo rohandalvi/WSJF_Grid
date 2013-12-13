d8b63030a24006bb0154a18df6daba02da825621
Rohan <rohan.dalvi@me.com>
2013-12-13 15:00:40 -0500

    bug fixes where 1. 'Score' column could not be sorted. 2. 'Decimal' values for score supported that rounds it off to 3 decimal places.

--- App.js
+++ App.js
@@ -1,119 +1,108 @@
-var Ext = window.Ext4 || window.Ext;
-
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
 
-    // entry point to app
     launch: function() {
-        this._doLayout();
-    },
-
-    //initial layout of widgets
-    _doLayout: function() {
+        //Write app code here
         this._loadFeatures();
     },
-
-    // load features into store
-    _loadFeatures: function() {
-        window.console && console.log("Loading features");
-
-        Ext.create('Rally.data.WsapiDataStore', {
-            model: 'PortfolioItem/Feature',
-            autoLoad: true,
-            listeners: {
-                load: function(store, records, success) {
-                    console.log("Loaded Store with %i records", records.length);
-                    this._calculateScore(records); // make sure the scores are up to date
-                    this._updateGrid(store); // populate the grid
-                },
-
-                update: function(store, rec, modified, opts) {
-                    console.log("data updated");
-                    this._calculateScore([rec]); // only update the record (feature) that changed
-                },
-                scope: this
-            },
-
-            fetch: ['Name', 'FormattedID', 'JobSize', 'TimeValue', 'OERR', 'UserValue', 'Score']
-        });
+    
+    _loadFeatures: function() { 
+    	var that = this;
+    	 console.log("Loading Features");
+    	 Ext.create('Rally.data.WsapiDataStore',{
+    	 	model: 'PortfolioItem/Feature',
+    	 	autoLoad: true,
+    	 	remoteSort: false,
+    	 	listeners:{
+    	 		load: function(store,records,success){
+    	 			if(records!=null){
+    	 			console.log("Loaded store with %i records",records.length);
+    	 			Ext.Array.each(records,function(feature){
+    	 				
+    	 				feature.set("Score",that._checkValue((feature.get("c_TimeValue")+feature.get("c_UserValue"))/feature.get("c_JobSize")));
+    	 			});
+    	 			
+    	 			this._updateGrid(store);
+    	 			}
+    	 	},
+    	 		
+    	 		scope: this
+    	 	},
+    	 	fetch: ['Name','FormattedID','c_JobSize','c_TimeValue','c_UserValue']
+    	 });
     },
-
-    _calculateScore: function(records) {        
-        Ext.Array.each(records, function(feature) {
-            // get the data used to calc score
-            // if poor performance is an issue, you can try taking off the parseInt casts
-            var jobSize = parseInt(feature.data.JobSize + "", 10); // parse int ensures we are dealing with ints, base 10
-            var timeValue = parseInt(feature.data.TimeValue + "", 10);
-            var OERR = parseInt(feature.data.OERR + "", 10);
-            var userValue = parseInt(feature.data.UserValue + "", 10);
-
-            if (jobSize > 0) { // jobSize is the denominator so make sure it's not 0
-                var score = ~~ (((userValue + timeValue + OERR) / jobSize) + 0.5); // shortcut for casting to int
-                
-                if (parseInt(feature.get("Score") + "", 10) !== score) { // only update if score changed
-                    feature.set('Score', score); // set score value in db
-                    // don't call feature.save(); it just breaks stuff
-                    console.log("Setting a new score");
-                }
-            }
-        });
+    _checkValue: function(value){
+    	if(isNaN(value))
+    		return -1;
+    	else 
+    	 return Math.round(value*1000)/1000;
+    },
+    _createGrid: function(myStore){
+    	console.log("Load up a populated grid! ");
+    	this._myGrid = Ext.create('Rally.ui.grid.Grid',{
+    		xtype: 'rallygrid',
+    		title: 'Feature Scoping Grid',
+    		height: 500,
+    		store: myStore,
+    		enabelEditing: true,
+    		remoteSort: false,
+    		selType: 'cellmodel',
+    		columnCfgs:[{
+    			text: 'Portfolio ID',
+    			dataIndex: 'FormattedID',
+    			flex: 1,
+    			xtype: 'templatecolumn',
+    			tpl: Ext.create('Rally.ui.renderer.template.FormattedIDTemplate'),
+    			sortable: true
+    		},{
+    			text: 'Name',
+    			dataIndex: 'Name',
+    			flex: 2,
+    			sortable: true
+    		},{
+    			text: 'Time Value',
+    			
+    			dataIndex: 'c_TimeValue',
+    			sortable: true
+    		},{
+    			text: 'Job Size',
+    			dataIndex: 'c_JobSize',
+    			sortable: true
+    		},{
+    			text: 'User Value',
+    			dataIndex: 'c_UserValue',
+    			sortable: true
+    		},{
+    			text: 'Score',
+    			dataIndex: 'Score',
+    			remoteSort: false,
+    			doSort: function(state){
+    				var ds = this.up('grid').getStore();
+    				var field = this.getSortParam();
+    				ds.sort({
+    					property: field,
+    					direction: state,
+    					
+    					sorterFn: function(v1,v2){
+    						v1 = v1.get(field);
+    						v2 = v2.get(field);
+    						return v1 > v2 ? 1 : (v1 < v2? -1 : 0);
+    					}
+    				});
+    				
+    			}
+    		}]
+    	});
+    	this.add(this._myGrid);
     },
     
-    _createGrid: function(myStore) {
-        console.log("Load up a populated grid!", myStore);
-        this._myGrid = Ext.create('Rally.ui.grid.Grid', {
-            xtype: 'rallygrid',
-            title: 'Feature Scoring Grid',
-            height: 200,
-            store: myStore,
-            enableEditing: true,
-            selType: 'cellmodel',
-            columnCfgs: [
-                { // override ID and Name - no changes on these allowed in this grid
-                    text: 'Portfolio ID',
-                    dataIndex: 'FormattedID',
-                    flex: 1,
-                    xtype: 'templatecolumn',
-                    tpl: Ext.create('Rally.ui.renderer.template.FormattedIDTemplate') // make the ID a live link
-                },             
-                
-                { 
-                    text: "Name",
-                    dataIndex: "Name",
-                    flex: 2
-                }, 
-                
-                "TimeValue", 'OERR', 'UserValue', 'JobSize', // use native Ext formatting - allows cell edits & got rid of errors
-                
-                { // override score so that the user can't edit it
-                    text: "Score",
-                    dataIndex: 'Score'
-                }
-            ]
-        });
-        this.add(this._myGrid);
-        
-        // override the event publish to prevent random refreshes of the whole app when the cell changes
-        var celledit = this._myGrid.plugins[0];
-        var oldPub = celledit.publish;
-        var newPub = function (event, varargs) {  
-            if (event !== "objectupdate") {
-                oldPub.apply(this, arguments);
-            } else {
-                // no-op
-            }
-        };
-        
-        celledit.publish = Ext.bind(newPub, celledit);
-    },
-    _updateGrid: function(myStore) {
-        if (this._myGrid === undefined) {
-            this._createGrid(myStore);
-        }
-        else {
-            this._myGrid.reconfigure(myStore);
-        }
+    _updateGrid: function(myStore){
+    	if(this._myGrid === undefined)
+    		this._createGrid(myStore);
+    	else
+    		this._myGrid.reconfigure(myStore);
     }
-
 });
+
+
